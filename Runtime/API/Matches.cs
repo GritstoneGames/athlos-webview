6af7ce4922848f1692d1ff1843806a4b3c4383ff
James Battersby <james.battersby@gritstone.games>
2022-12-13 15:42:30 +0000

    Updated MatchResult reporting call

--- Runtime/API/Matches.cs
+++ Runtime/API/Matches.cs
@@ -1,18 +1,15 @@
 //TODO [ATH-1562] License
 
-using Athlos.Models.Client.Matches;
-using Athlos.API.Common.Matches;
+using Athlos.Models.Common.Matches;
 using System;
 
 namespace Athlos.API.WebView
 {
   public partial class AthlosClientAPI
   {
-    public static void ReportMatchResult(Match match, Action onSuccess, Action<AthlosError[]> onFail)
+    public static void ReportMatchResult(MatchResult match, Action onSuccess, Action<AthlosError[]> onFail)
     {
-      string url = $"match-series/{match.Id}/matches/1/results";
-      AthlosAPI.Patch(AthlosAPI.MatchesCategory, url, 
-        new ReportMatchResultRequest(match), onSuccess, onFail);
+      AthlosAPI.Patch(AthlosAPI.MatchesCategory, match.Url, match, onSuccess, onFail);
     }
   }
 }
