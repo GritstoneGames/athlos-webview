2a64650f23947ec60985e77d5457e3d6bd0f9d29
James Battersby <james.battersby@gritstone.games>
2022-12-19 18:25:46 +0000

    Created MatchResult form

--- Runtime/API/Matches.cs
+++ Runtime/API/Matches.cs
@@ -1,15 +1,32 @@
 //TODO [ATH-1562] License
 
-using Athlos.Models.Common.Matches;
 using System;
 
 namespace Athlos.API.WebView
 {
   public partial class AthlosClientAPI
   {
+    public class MatchResult
+    {
+      private Models.Common.Matches.MatchResult result;
+      private string matchSeriesId;
+      private int matchNumber;
+
+      public MatchResult(Models.Common.Matches.MatchResult result, string matchSeriesId, int matchNumber)
+      {
+        this.result = result;
+        this.matchSeriesId = matchSeriesId;
+        this.matchNumber = matchNumber;
+      }
+
+      public Models.Common.Matches.MatchResult Result { get { return result; } }
+      public string MatchSeriesId { get { return matchSeriesId; } }
+      public int MatchNumber { get { return matchNumber; } }
+    }
+
     public static void ReportMatchResult(MatchResult match, Action onSuccess, Action<Error[]> onFail)
     {
-      AthlosAPI.Patch(AthlosAPI.MatchesCategory, match.Url, match, onSuccess, onFail);
+      AthlosAPI.Patch(AthlosAPI.MatchesCategory, Models.Common.Matches.MatchResult.Url(match.MatchSeriesId, match.MatchNumber), match.Result, onSuccess, onFail);
     }
   }
 }
