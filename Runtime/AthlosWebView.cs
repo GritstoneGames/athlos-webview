84a21d1f0edc64dff147e0586f3fdc9aff191c8f
James Battersby <james.battersby@gritstone.games>
2022-12-22 13:36:55 +0000

    Added RGB colour configuration for loading Athlos urls

--- Runtime/AthlosWebView.cs
+++ Runtime/AthlosWebView.cs
@@ -16,8 +16,44 @@ namespace Athlos.WebView
       Release
     }
 
+    [Serializable]
+    struct InitialColors
+    {
+      private const string HexFormat = "x2";
+
+      public bool enabled;
+      public Color background;
+      public Color spinner;
+
+      private string ToHex(Color color)
+      {
+        int r = (int)(color.r * 255f);
+        int g = (int)(color.g * 255f);
+        int b = (int)(color.b * 255f);
+
+        return $"{r.ToString(HexFormat)}{g.ToString(HexFormat)}{b.ToString(HexFormat)}";
+      }
+
+      public string BackgroundHex
+      {
+        get
+        {
+          return $"{ToHex(background)}";
+        }
+      }
+
+      public string SpinnerHex
+      {
+        get
+        {
+          return $"{ToHex(spinner)}";
+        }
+      }
+    }
+
     [Header("Configuration")]
     [SerializeField] private Scope scope;
+    [SerializeField] private InitialColors initialColors;
 
     public bool PageLoaded { get; protected set; }
     private UnityEvent<string> onMessageReceived;
@@ -26,12 +62,13 @@ namespace Athlos.WebView
     {
       get
       {
+        string initialColorsParam = initialColors.enabled ? $"&bg={initialColors.BackgroundHex}&spinner={initialColors.SpinnerHex}" : "";
         return 
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
-        scope == Scope.ProofOfConcept ? $"https://test.athlos.gg?tenant={AthlosHTTP.Tenant}" :
+        scope == Scope.ProofOfConcept ? $"https://test.athlos.gg?tenant={AthlosHTTP.Tenant}{initialColorsParam}" :
 #endif
-        scope == Scope.StagingInApp ?   $"https://staging-inapp.athlos.gg?tenant={AthlosHTTP.Tenant}" :
-                                        $"https://ingame.gfinity.gg?tenant={AthlosHTTP.Tenant}";
+        scope == Scope.StagingInApp ?   $"https://staging-inapp.athlos.gg?tenant={AthlosHTTP.Tenant}{initialColorsParam}" :
+                                        $"https://ingame.gfinity.gg?tenant={AthlosHTTP.Tenant}{initialColorsParam}";
       }
     }
 
